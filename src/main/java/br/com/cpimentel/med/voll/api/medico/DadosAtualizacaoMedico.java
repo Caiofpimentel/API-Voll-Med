8b3205826c44cba1e4f5331352483844f3d58ee0
Caio <caio-pimentel@hotmail.com>
2024-03-13 12:50:28 -0300

    adiciona excluir e atualizar medico

---
+++ src/main/java/br/com/cpimentel/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,9 @@
+package br.com.cpimentel.med.voll.api.medico;
+
+import br.com.cpimentel.med.voll.api.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoMedico(@NotNull Long id, String nome, String telefone, DadosEndereco endereco) {
+
+
+}
--- src/main/java/br/com/cpimentel/med/voll/api/medico/dadosListagemMedico.java
+++ src/main/java/br/com/cpimentel/med/voll/api/medico/dadosListagemMedico.java
@@ -1,4 +1,8 @@
 package br.com.cpimentel.med.voll.api.medico;
 
-public record dadosListagemMetodo(String nome, String email, String crm, Especialidade especialidade) {
+public record dadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+
+    public dadosListagemMedico(Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
 }
